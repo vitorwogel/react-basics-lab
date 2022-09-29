48de1ef992c78d6a5bc5c08b2bbf8b39eeffc86a
Vitor Wogel <vitorandre26@gmail.com>
2022-09-29 11:53:28 +0100

    Exercise 1 completed

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,7 +3,30 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <>  
+      <h1>HDip Computer Science - Module Table</h1>
+      <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr>
+          <td>DevOps</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>Web App Dev 2</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+      </tbody >
+    </table>
+    </>
   );
 };
 
