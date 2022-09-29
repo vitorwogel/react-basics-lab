3fc381c235e01070d968cd99376cdeb04f75e8da
Vitor Wogel <vitorandre26@gmail.com>
2022-09-29 12:14:54 +0100

    Exercise 3 completed

--- src/components/exercises/03_props.js
+++ src/components/exercises/03_props.js
@@ -2,9 +2,32 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = (props) => {
   return (
-      <h1>TODO</h1>
+    <>  
+    <h1>{props.course}</h1>
+    <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>No lectures</th>
+        <th>No practicals</th>
+      </tr>
+    </thead>
+    <tbody >
+      <tr>
+        <th>{props.modules[0].name}</th>
+        <th>{props.modules[0].noLectures}</th>
+        <th>{props.modules[0].noPracticals}</th>
+      </tr>
+      <tr>
+        <th>{props.modules[1].name}</th>
+        <th>{props.modules[1].noLectures}</th>
+        <th>{props.modules[1].noPracticals}</th>
+      </tr>
+    </tbody >
+  </table>
+  </>
   );
 };
 
